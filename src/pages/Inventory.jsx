cb15de7760dddd83e781c2b329f6c3a59586a9fc
chamchimayo <bazzyungfore@gmail.com>
2025-03-18 11:46:12 +0900

    why????

--- src/pages/Inventory.jsx
+++ src/pages/Inventory.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import "../styles/Inventory.css"; // ✅ 스타일 추가
+import "../styles/Inventory.css";
 
 const Inventory = () => {
   const [inventoryData, setInventoryData] = useState([]);
